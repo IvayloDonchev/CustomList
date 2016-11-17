f12ac2960bef6eee35b1e884073f70978c6decaa
Ivaylo Donchev <idonchev@hotmail.com>
2016-11-17 13:35:10 +0200

    Add project files.

---
+++ CustomList/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using GenericList;
+
+namespace CustomList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenList<int> l = new GenList<int>();
+            l.Add(1);
+            l.Add(2);
+            l.Add(3);
+            l.Add(4);
+            l.Disp();
+
+            GenList<int> l2 = (GenList<int>) l.Clone();
+
+            foreach (int x in l)
+                Console.Write($"{x} ");
+            Console.WriteLine();
+            Console.WriteLine(l[2]);
+            l[2] = 10;
+            l.Disp();
+            l2.Disp();
+            ArrayList arrL = l; // implicit conversion
+            foreach (var x in arrL)
+                Console.Write($"{x} ");
+            Console.WriteLine();
+
+            
+
+        }
+    }
+}
